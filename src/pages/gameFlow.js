ce2e1ac995ec8ffde44ffd27216fdbfa26faffb4
Andrew Pieschke <ampieschke@gmail.com>
2022-01-19 16:46:45 -0500

    updated styling, added header image

--- src/pages/gameFlow.js
+++ src/pages/gameFlow.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 import Scoreboard from "../components/scoreboard/scoreboard";
 import PhaseInfo from "../components/phaseInfo/phaseInfo";
 import ArmyList from "../components/armyList/armyList";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
+import Headline from "./kttrackerraw.png";
 
 class GameFlow extends Component {
   constructor(props) {
@@ -26,44 +27,61 @@ class GameFlow extends Component {
     }
     return (
       <div>
+        <img src={Headline} className="img-fluid" alt="Logo" />
         <Scoreboard />
         {mainDisplay}
         <div className="bottomPart text-center">
           <Row>
             <Col xs="4" className="footicon ">
-              <button
+              <Button
                 onClick={this.displayUpdate}
-                className=""
+                className="footBtn"
                 value="phases"
                 id="phases"
               >
-                {/* <i
-                  className="fas fa-book-open fa-2x"
-                  // style={{ color: "white" }}
-                ></i> */}
+                <Row>
+                  <i className="fas fa-book-open fa-2x"></i>
+                </Row>
                 General Rules
-              </button>
+              </Button>
             </Col>
             <Col xs="4" className="footicon">
-              <button
+              <Button
                 onClick={this.displayUpdate}
-                className=""
+                className="footBtn"
                 value="armylist"
                 id="armylist"
               >
+                <Row>
+                  <i className="fas fa-trophy fa-2x"></i>
+                </Row>
                 Army Lists
-              </button>
+              </Button>
             </Col>
             <Col xs="4" className="footicon">
               <button
                 onClick={this.displayUpdate}
-                className=""
+                className="footBtn"
                 value="/"
                 id="/"
               >
                 Scenario Generator
               </button>
             </Col>
+            {/* <Col>
+              <span
+                onClick={this.displayUpdate}
+                className=""
+                value="phases"
+                id="phases"
+              >
+                hi
+                <i
+                  className="fas fa-trophy fa-2x"
+                  style={{ color: "white" }}
+                ></i>
+              </span>
+            </Col> */}
           </Row>
         </div>
       </div>
